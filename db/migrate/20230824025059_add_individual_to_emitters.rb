94f5dfbdc88926a9a9d50b0ca9d147edd11883c4
Joop Stark <stark.joop@gmail.com>
2023-08-24 09:17:05 -0600

    comments: added comments to database

--- db/migrate/20230824025059_add_individual_to_emitters.rb
+++ db/migrate/20230824025059_add_individual_to_emitters.rb
@@ -3,3 +3,5 @@ class AddIndividualToEmitters < ActiveRecord::Migration[7.0]
     add_reference :emitters, :individual, null: false, foreign_key: true
   end
 end
+
+# rails generate migration AddIndividualToEmitters individual:references
