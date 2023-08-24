41f1a5bf1fe3add73f2b6e688a646c577091a520
Joop Stark <stark.joop@gmail.com>
2023-08-23 21:34:58 -0600

    test: individual index page

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "individual index page", type: :feature do
+  it "can see all individuals and some attributes" do
+    anne = Individual.create(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
+    blair = Individual.create(email_display: "blair@aol.com", name: "Blair", age: 32, improve:true)
+    connor = Individual.create(email_display: "Connor@aol.com", name: "Connor", age: 33, improve:false)
+     
+    visit "/individuals"
+
+    expect(page).to have_content(anne.email_display)
+    expect(page).to have_content(anne.name)
+    expect(page).to have_content(blair.age)
+    expect(page).to have_content(connor.improve)
+  end
+
+
+
+
+end
+  
