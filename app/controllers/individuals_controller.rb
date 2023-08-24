de3d870f8c906610141f96dd04e6b8a7ed623ef7
Joop Stark <stark.joop@gmail.com>
2023-08-23 22:22:25 -0600

    test: basic show page

--- app/controllers/individuals_controller.rb
+++ app/controllers/individuals_controller.rb
@@ -2,4 +2,8 @@ class IndividualsController < ApplicationController
   def index
     @individuals = Individual.all
   end
+
+  def show
+    @individual = Individual.find(params[:id])
+  end
 end
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -14,6 +14,19 @@ RSpec.describe "individual index page", type: :feature do
     expect(page).to have_content(connor.improve)
   end
 
+  it "can see on individual and some attributes" do
+    anne = Individual.create(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
+    blair = Individual.create(email_display: "blair@aol.com", name: "Blair", age: 32, improve:true)
+    connor = Individual.create(email_display: "Connor@aol.com", name: "Connor", age: 33, improve:false)
+     
+    visit "/individuals/#{anne.id}"
+
+    expect(page).to have_content(anne.email_display)
+    expect(page).to have_content(anne.name)
+    expect(page).to have_content(anne.age)
+    expect(page).to_not have_content(connor.improve)
+  end
+
 
 
 
