cbe17e7f25b0e01cbb547316fdacfababa7d6f9d
Joop Stark <stark.joop@gmail.com>
2023-08-26 00:06:20 -0600

    feat: link to individual and emitter index in individualemitter index

--- spec/features/individuals/emitters/index_spec.rb
+++ spec/features/individuals/emitters/index_spec.rb
@@ -11,4 +11,27 @@ RSpec.describe "Individual's emitters index" do
     expect(page).to have_content(vehicle.appliance)
     expect(page).to have_content(hot_water.appliance)
   end
+
+  it "links to the individual index on emitter index" do
+    anne = Individual.create!(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
+    vehicle = anne.emitters.create!(appliance: "Yukon", co2e_per_hour: 100, hours_per_day: 3, use: true)
+    hot_water = anne.emitters.create!(appliance: "On Demand Water Heater", co2e_per_hour: 12, hours_per_day: 0.5, use: true)
+
+    visit "/individuals/#{anne.id}/emitters"
+    click_on "All Individuals"
+
+    expect(current_path).to eq("/individuals")
+  end
+
+  it "links to the individual index on emitter index" do
+    anne = Individual.create!(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
+    vehicle = anne.emitters.create!(appliance: "Yukon", co2e_per_hour: 100, hours_per_day: 3, use: true)
+    hot_water = anne.emitters.create!(appliance: "On Demand Water Heater", co2e_per_hour: 12, hours_per_day: 0.5, use: true)
+
+    visit "/individuals/#{anne.id}/emitters"
+    click_on "All Emitters"
+
+    # save_and_open_page
+    expect(current_path).to eq("/emitters")
+  end
 end
