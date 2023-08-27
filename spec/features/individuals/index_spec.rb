360381420e4759875564b1ae6826537d93f1207c
Joop Stark <stark.joop@gmail.com>
2023-08-26 21:32:47 -0600

    tests and feat: individuals index and show have links to individual index and show

--- spec/features/individuals/index_spec.rb
+++ spec/features/individuals/index_spec.rb
@@ -46,6 +46,41 @@ RSpec.describe "individual index page", type: :feature do
 
   it "puts the newest individual on top"
 
+  it "links to the individual index on index show" do
+    anne = Individual.create!(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
+    vehicle = anne.emitters.create!(appliance: "Yukon", co2e_per_hour: 100, hours_per_day: 3, use: true)
+    hot_water = anne.emitters.create!(appliance: "On Demand Water Heater", co2e_per_hour: 12, hours_per_day: 0.5, use: true)
+
+    visit "/individuals/#{anne.id}"
+    click_on "All Individuals"
+
+    expect(current_path).to eq("/individuals")
+  end
+
+  it "links to the emitter index on individual index" do
+    anne = Individual.create!(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
+    vehicle = anne.emitters.create!(appliance: "Yukon", co2e_per_hour: 100, hours_per_day: 3, use: true)
+    hot_water = anne.emitters.create!(appliance: "On Demand Water Heater", co2e_per_hour: 12, hours_per_day: 0.5, use: true)
+
+    visit "/individuals"
+    click_on "All Emitters"
+
+    # save_and_open_page
+    expect(current_path).to eq("/emitters")
+  end
+
+  it "links to the individual index on individual show" do
+    anne = Individual.create!(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
+    vehicle = anne.emitters.create!(appliance: "Yukon", co2e_per_hour: 100, hours_per_day: 3, use: true)
+    hot_water = anne.emitters.create!(appliance: "On Demand Water Heater", co2e_per_hour: 12, hours_per_day: 0.5, use: true)
+
+    visit "/individuals/#{anne.id}"
+    click_on "All Emitters"
+
+    # save_and_open_page
+    expect(current_path).to eq("/emitters")
+  end
+
 
 
 
