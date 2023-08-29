30b35fb920af0b2fd7e14de183a57733ad384950
Joop Stark <stark.joop@gmail.com>
2023-08-28 22:19:28 -0600

    feat: add emitters to individual

--- app/controllers/individual_emitters_controller.rb
+++ app/controllers/individual_emitters_controller.rb
@@ -1,5 +1,6 @@
 class IndividualEmittersController < ApplicationController
   def index
+    # binding.pry
     @individual = Individual.find(params[:individual_id])
     @emitters = @individual.emitters
   end
@@ -10,9 +11,9 @@ class IndividualEmittersController < ApplicationController
 
   def create
     @individual = Individual.find(params[:individual_id])
-    @individual.emitters.create(emitter_params)
+    @individual.emitters.create!(emitter_params)
 
-    # redirect_to "/individuals/#{individual.id}"
+    redirect_to "/individuals/#{@individual.id}/emitters"
   end
 
   private
