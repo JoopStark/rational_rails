a898832a105b4edd9fa3b1803d0119bf1fff44ef
Joop Stark <stark.joop@gmail.com>
2023-08-29 17:24:37 -0600

    feat:working on conditional index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+anne = Individual.create!(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
+blair = Individual.create!(email_display: "blair@aol.com", name: "Blair", age: 32, improve:true)
+connor = Individual.create!(email_display: "Connor@aol.com", name: "Connor", age: 33, improve:false)
+vehicle = connor.emitters.create!(appliance: "Yukon", co2e_per_hour: 100, hours_per_day: 3, use: true)
+hot_water = connor.emitters.create!(appliance: "On Demand Water Heater", co2e_per_hour: 12, hours_per_day: 0.5, use: true)
+old_hot_water = anne.emitters.create!(appliance: "On Demand Water Heater", co2e_per_hour: 12, hours_per_day: 0.5, use: false)
