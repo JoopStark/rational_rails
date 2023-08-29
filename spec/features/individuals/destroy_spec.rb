7748426ec4f266b1047b0d82d68cfa175cb009f7
Joop Stark <stark.joop@gmail.com>
2023-08-29 15:58:35 -0600

    test: can delete from show page

--- spec/features/individuals/destroy_spec.rb
+++ spec/features/individuals/destroy_spec.rb
@@ -13,14 +13,27 @@ RSpec.describe "Destroy individual" do
     expect(page).to have_content(connor.name)
     
     click_button "Delete #{connor.name}"
-    
-    save_and_open_page
 
     expect(current_path).to eq("/individuals")
     expect(page).to_not have_content(connor.name)
   end
 
-  it "can delete individuals from index page" do
+  it "can delete individuals from show page" do
+    anne = Individual.create!(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
+    blair = Individual.create!(email_display: "blair@aol.com", name: "Blair", age: 32, improve:true)
+    connor = Individual.create!(email_display: "Connor@aol.com", name: "Connor", age: 33, improve:false)
+    vehicle = connor.emitters.create!(appliance: "Yukon", co2e_per_hour: 100, hours_per_day: 3, use: true)
+    hot_water = connor.emitters.create!(appliance: "On Demand Water Heater", co2e_per_hour: 12, hours_per_day: 0.5, use: true)
+
+    visit "/individuals/#{connor.id}"
+    
+    click_button "Delete #{connor.name}"
+    
+    expect(current_path).to eq("/individuals")
+    expect(page).to_not have_content(connor.name)
+  end
+
+  it "destroys emitters for individual when individual is deleted" do
     anne = Individual.create!(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
     blair = Individual.create!(email_display: "blair@aol.com", name: "Blair", age: 32, improve:true)
     connor = Individual.create!(email_display: "Connor@aol.com", name: "Connor", age: 33, improve:false)
