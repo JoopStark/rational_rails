665d5fedda2b5849aa3e14a11e1a33126dfd73a6
Joop Stark <stark.joop@gmail.com>
2023-08-28 22:19:02 -0600

    test: add emitters to individual

--- spec/features/individuals/emitters/new_spec.rb
+++ spec/features/individuals/emitters/new_spec.rb
@@ -7,11 +7,29 @@ RSpec.describe "individual's emitters page" do
     visit "/individuals/#{anne.id}/emitters/"
 
     click_link "Add an emitter for #{anne.name}"
-    save_and_open_page
+    # save_and_open_page
     expect(current_path).to eq("/individuals/#{anne.id}/emitters/new")
   end
 
+  it "can create a new emitter attached to an individual" do
+    anne = Individual.create!(email_display: "AnneSmith12@aol.com", name: "Anne Smith", age: 31, improve:true)
 
+    visit "/individuals/#{anne.id}/emitters/new"
 
+    fill_in("Name or Type", with: "Gas Stove")
+    fill_in("CO2e/hour", with: 2.1 )
+    fill_in("Hours/day", with: 3.3 )
+    # check("Still being used?")
+    click_button ('Add Emitter')
+    
+    
+    new_emitter_id = anne.emitters.last.id
+    expect(current_path).to eq("/individuals/#{anne.id}/emitters")
+    # save_and_open_page
+    expect(page).to have_content("Gas Stove")
+    expect(page).to have_content(2.1)
+    expect(page).to have_content(3.3)
+    expect(page).to have_content(false)
+  end
 end
 
